e7b5a451bf5e73f01dfdcd6a0c256085b91914b3
Lost_Shadow <sobolm58@gmail.com>
2023-12-05 22:10:25 +0700

    add lab 6 math

---
+++ ВычМат/Lab6/Lab5/Source/FunctionHolder.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab5.Source
+{
+    public class FunctionHolder
+    {
+        public float GetFunction(double x)
+        {
+            return (float)(x*x - 4);
+        }
+
+        public float GetDifferential(float x)
+        {
+            var diff = 0.000001f;
+            return (GetFunction(x + diff) - GetFunction(x - diff)) / (2 * diff);
+        }
+
+        public float Get2Differential(float x)
+        {
+            var diff = 0.000001f;
+            return (GetDifferential(x + diff) - GetDifferential(x - diff)) / (2 * diff);
+        }
+    }
+}
